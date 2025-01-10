da937f4000a0198ee03cd6ac7ef0a26b169a3c81
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-01-10 12:09:36 -0500

    feat(http): added base http file that allows the conection with the backend

---
+++ src/services/http-common.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:4000";
+
+const http = axios.create({
+    baseURL: API_URL,
+    headers: {'Content-Type': 'application/json'}
+});
+
+export default http;
+
