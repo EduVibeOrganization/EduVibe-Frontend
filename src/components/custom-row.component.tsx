bfa0284793f2e67c3caa7934aab5fd214c348f56
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-01-09 22:53:41 -0500

    feat(conference): added custom row component

---
+++ src/components/custom-row.component.tsx
@@ -0,0 +1,13 @@
+interface ICustomRow {
+    label: string;
+    component: React.ReactNode;
+}
+
+export function CustomRow ({ label, component }: ICustomRow) {
+    return (
+        <div className="flex items-center w-full gap-5">
+            <h1 className="w-1/3 text-right">{label}</h1>
+            <div className="w-2/3">{component}</div>
+        </div>
+    );
+}
--- src/pages/conference-creation.tsx
+++ src/pages/conference-creation.tsx
@@ -6,28 +6,26 @@ import { CustomInputText } from "@/components/custom-input-text.component";
 import { CustomSelectorComponent } from "@/components/custom-selector.component";
 import { useState } from "react";
 import { CustomInputSwitch } from "@/components/custom-input-switch.component";
+import { CustomRow } from "@/components/custom-row.component";
 
 function ConferenceCreation(){
     const router = useRouter();
     const [privacy, setPrivacy] = useState<string>("privado");
-
     return(
         <div className="bg-gray-300 auth-page-container">
             <div className="flex flex-col justify-center items-center mt-10 gap-5 lg:ml-96">
-                <CustomInputText value={""} placeHolder={"Nombre"} onChange={(e) => console.log(e)}/>
-                <CustomInputText value={""} placeHolder={"Privacidad"} onChange={(e) => console.log(e)}/>
-                <div>
-                    <h1>Privacidad</h1>
-                    <CustomSelectorComponent
-                        value={privacy}
-                        options={["Privado", "Público"]}
-                        onChange={(value) => setPrivacy(value)}
-                    />
-                </div>
-                <div>
-                    <h1>Compartir Pantalla</h1>
-                    <CustomInputSwitch/>
-                </div>
+                <CustomRow 
+                label={"Nombre"} 
+                component={<CustomInputText value={""} placeHolder={"Nombre"} onChange={(e) => console.log(e)}/>}
+                />
+                <CustomRow 
+                    label={"Privacidad"} 
+                    component={ <CustomSelectorComponent value={privacy} options={["Privado", "Público"]} onChange={(value) => setPrivacy(value)}/> }
+                />
+                <CustomRow 
+                    label={"Compartir Pantalla"} 
+                    component={<CustomInputSwitch/>}
+                />
             </div>
         </div>
     );
