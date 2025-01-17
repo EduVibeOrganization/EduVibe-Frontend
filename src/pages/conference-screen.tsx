476a88249d5a720980d2d2ea02494b781f670e6e
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-01-17 02:23:44 -0500

    refactor(conference): simplified the way the code is structured and added a not functioning screen sharing feature

--- src/pages/conference-screen.tsx
+++ src/pages/conference-screen.tsx
@@ -9,17 +9,31 @@ import "../app/assets/styles/conference-screen.css";
 import router from 'next/router';
 
 function ConferenceScreen() {
-    const localSessionId = useLocalSessionId();
+    //Configuration
     const searchParams = useSearchParams();
     const callObject = useCallObject({});
-    const [participants, setParticipants] = useState<string[]>([]);
-    const [totalCount, setTotalCount] = useState<number>(0);
+    const screenShareState = useScreenShare();
+    const conferenceService = new ConferenceService();
+    
+    //Room info
     const [roomName, setRoomName] = useState<string | null>(null);
+    const [totalCount, setTotalCount] = useState<number>(0);
+    const [participantsSharingScreens, setParticipantsSharingScreens] = useState<string[]>([]);
+
+    //Participants of the room
+    const [participants, setParticipants] = useState<string[]>([]);
+    
+    //Conference controls
     const [isCameraOn, setIsCameraOn] = useState<boolean>(true);
     const [isMicOn, setIsMicOn] = useState<boolean>(true);
     const [isScreenSharing, setIsScreenSharing] = useState<boolean>(false);
-    const conferenceService = new ConferenceService();
-    const [screenShareId, setScreenShareId] = useState<string | null>(null);
+    
+    {/* Room Creation */}
+    /*
+        When the page is created, it fetches the name of the room
+        Having the name before anything else important because we need
+        it to join the room
+    */
     useEffect(() => {
         const room = searchParams?.get('room');
         if (room) {
@@ -27,6 +41,8 @@ function ConferenceScreen() {
         }
     }, [searchParams]);
 
+
+    //Fetches the participants using the room service
     const fetchParticipants = async () => {
         if (!roomName) return;
         const response = await conferenceService.getRoomByNameWithPresence(roomName);
@@ -38,41 +54,14 @@ function ConferenceScreen() {
         }
     };
 
-    const toggleCamera = () => {
-        if (callObject) {
-            callObject.setLocalVideo(!isCameraOn);
-            setIsCameraOn((prev) => !prev);
-        }
-    };
-
-    const toggleMic = () => {
-        if (callObject) {
-            callObject.setLocalAudio(!isMicOn);
-            setIsMicOn((prev) => !prev);
-        }
-    };
-
-    const toggleScreenShare = () => {
-        if (!callObject) return;
-        if(isScreenSharing){
-            setScreenShareId(null);
-            callObject.stopScreenShare(); 
-        } else {
-            setScreenShareId(localSessionId);
-            callObject.startScreenShare();
-        }
-    };
-
-    const leaveRoom = () => {
-        if (callObject) {
-            callObject.leave();
-            callObject.destroy();
-        }
-        router.push('/sign-in');
-    };
-
+    /* 
+        The name obtained is used to create the url.
+        The other functions are used to handle the events of the callObject
+        Every time a participants enters or leaves the room, fetchParticipants is called
+    */
     useEffect(() => {
         if (callObject && roomName) {
+            
             const roomUrl = `https://handin.daily.co/${roomName}`;
             
             callObject.join({ url: roomUrl });
@@ -80,22 +69,12 @@ function ConferenceScreen() {
             callObject.on('participant-updated', fetchParticipants);
             callObject.on('participant-left', fetchParticipants);
 
-            callObject.on('participant-updated', () => {
-                const state = useScreenShare();
-                setScreenShareId(state.screens[0]?.session_id);
-                fetchParticipants();
-            });
-
             callObject.on('local-screen-share-started', () => {
-                setScreenShareId(localSessionId);
                 setIsScreenSharing(true);
-                alert('Compartiendo pantalla');
             });
 
             callObject.on('local-screen-share-stopped', () => {
-                setScreenShareId(null);
                 setIsScreenSharing(false);
-                alert('Dejó de compartir pantalla');
             });
         }
 
@@ -106,6 +85,54 @@ function ConferenceScreen() {
             callObject?.off('participant-left', fetchParticipants);
         };
     }, [callObject, roomName]);
+
+    {/* Room Deletion */}
+    /*
+        This useEffect is used to handle the event of the user leaving the room
+        It is used to prevent the user to enter twice to the room
+     */
+    useEffect(() => {
+        window.addEventListener('beforeunload', handleBeforeUnload);
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+    }, [callObject]);
+
+    //Destroys the call object and removes the user from the room
+    const handleBeforeUnload = () => {
+        if (callObject) {
+            callObject.leave();
+            callObject.destroy();
+        }
+    };
+
+    const leaveRoom = () => {
+        handleBeforeUnload
+        router.push('/conference-left');
+    };
+
+    {/* Conference controls */}
+    const toggleCamera = () => {
+        if (callObject) {
+            callObject.setLocalVideo(!isCameraOn);
+            setIsCameraOn((prev) => !prev);
+        }
+    };
+    const toggleMic = () => {
+        if (callObject) {
+            callObject.setLocalAudio(!isMicOn);
+            setIsMicOn((prev) => !prev);
+        }
+    };
+    const toggleScreenShare = () => {
+        if(callObject) {
+            if (!isScreenSharing) {
+                callObject.startScreenShare();
+            } else {
+                callObject.stopScreenShare();
+            }
+        }
+    };
 
     return (
         <div className='content-background'>
@@ -132,21 +159,21 @@ function ConferenceScreen() {
                                     automirror
                                 />
                             ))}
-                            { screenShareId != null && isScreenSharing && (
+                            {participantsSharingScreens.map((id: string) => (
                                 <DailyVideo
-                                    key={screenShareId}
-                                    sessionId={screenShareId}
+                                    key={id}
+                                    sessionId={id}
                                     type="screenVideo"
                                     style={{
-                                        width: '640px',
-                                        height: '360px',
+                                        width: '300px',
+                                        height: '200px',
                                         margin: '10px',
                                         borderRadius: '10px',
-                                        border: '2px solid #f97316',
                                         backgroundColor: '#000'
                                     }}
+                                    automirror
                                 />
-                            )}
+                            ))}
                         </div>
                     </DailyProvider>
                 </div>
@@ -182,6 +209,14 @@ function ConferenceScreen() {
                         color={'red'}
                         size='small'
                         onSubmit={leaveRoom}
+                    />
+                    <CustomButtonDX 
+                        title={'Participantes compartiendo pantalla'}
+                        icon='pi pi-users'
+                        iconPosition='right'
+                        color={'green'}
+                        size='small'
+                        onSubmit={() => {console.log(participantsSharingScreens)}}
                     />
                 </div>
             </div>
