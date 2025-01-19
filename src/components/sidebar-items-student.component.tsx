621c5c9d2ecf38ef914dc63209137d1938e6cd3c
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-01-19 15:49:24 -0500

    chore(student): updated the student sidebar to not include buttons that direct you to prohibited pages

--- src/components/sidebar-items-student.component.tsx
+++ src/components/sidebar-items-student.component.tsx
@@ -11,12 +11,9 @@ export function SidebarItemsStudent(){
         <div className="sidebar-content-items">
             <SidebarItem2 icon={"pi pi-home"} title={"Inicio"} textColor={textColor} backgroundAccentColor={backgroundAccentColor} textAccentColor={textAccentColor} onClick={() => router.push("/home-student")}/>
             <SidebarItem2 icon={"pi pi-book"} title={"Cursos"} textColor={textColor} backgroundAccentColor={backgroundAccentColor} textAccentColor={textAccentColor} onClick={() => router.push("/courses")}/>    
-            <SidebarItem2 icon={"pi pi-video"} title={"Videoconferencias"} textColor={textColor} backgroundAccentColor={backgroundAccentColor} textAccentColor={textAccentColor} onClick={() => router.push("/conference-creation")}/>    
             <SidebarItem2 icon={"pi pi-receipt"} title={"Transacciones"} textColor={textColor} backgroundAccentColor={backgroundAccentColor} textAccentColor={textAccentColor} onClick={() => router.push("/home-student")}/>
             <SidebarItem2 icon={"pi pi-question-circle"} title={"Centro de Ayuda"} textColor={textColor} backgroundAccentColor={backgroundAccentColor} textAccentColor={textAccentColor} onClick={() => router.push("/help-center-student")}/>    
             <SidebarItem2 icon={"pi pi-user"} title={"Perfil"} textColor={textColor} backgroundAccentColor={backgroundAccentColor} textAccentColor={textAccentColor} onClick={() => router.push("/my-profile")}/>
-            <SidebarItem2 icon={"pi pi-cog"} title={"Administración"} textColor={textColor} backgroundAccentColor={backgroundAccentColor} textAccentColor={textAccentColor} onClick={() => router.push("/admin-authorization")}/>    
-            <SidebarItem2 icon={"pi pi-graduation-cap"} title={"Profesor"} textColor={textColor} backgroundAccentColor={backgroundAccentColor} textAccentColor={textAccentColor} onClick={() => router.push("/courses-teacher")}/>    
         </div>
     );
 }
