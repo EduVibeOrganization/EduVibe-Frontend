28ebc47a1a996ff8aa61b331301d3422f2ff41c4
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-01-20 00:06:43 -0500

    style(admin): updated the admin event page styles

--- src/pages/admin-event.tsx
+++ src/pages/admin-event.tsx
@@ -1,82 +1,82 @@
 import { CustomSidebarDX } from "@/components/custom-sidebar-dx.component";
 import { SidebarItemsAdmin } from "@/components/sidebar-items-admin.component";
 import { useState, useEffect } from "react";
+import { CourseDTO } from "@/models/course.dto";
+import { CourseService } from "@/services/course.service";
+import { Paginator } from "primereact/paginator";
+import CourseCard from "@/components/course-card";
 
 import "../app/assets/styles/public.css";
-import "../app/assets/styles/admin-event.css";
-
-const fakeCourses = [
-    {
-        id: 1,
-        title: "Curso de JavaScript Avanzado",
-        subtitle: "Domina el lenguaje más popular del mundo",
-        description: "Aprende conceptos avanzados de JavaScript y cómo aplicarlos.",
-        rating: 4.8,
-        startDate: new Date("2025-01-01"),
-        hours: 40,
-        banner: "https://cdn.prod.website-files.com/5f5a53e153805db840dae2db/6458d604829148153fb7cd52_codigo-python-beautiful-soup.jpg",
-        price: 99.99,
-    },
-    {
-        id: 2,
-        title: "Introducción a React",
-        subtitle: "Crea interfaces modernas y dinámicas",
-        description: "Un curso completo para aprender React desde cero.",
-        rating: 4.6,
-        startDate: new Date("2025-02-01"),
-        hours: 30,
-        banner: "https://www.codingdojo.la/wp-content/uploads/2022/07/react.jpg",
-        price: 0,
-    }
-];
+import "../app/assets/styles/admin.css";
 
 function AdminEvent(){
-    const [courses, setCourses] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedCourse, setSelectedCourse] = useState<CourseDTO | null>(null);
+    const [first, setFirst] = useState(0);
+    const [courseService] = useState(new CourseService());
+    const [courses, setCourses] = useState<CourseDTO[]>([]);
 
     useEffect(() => {
-    setCourses(fakeCourses);
+        courseService.getCoursesByPage(1, 6).then((response) => {
+            setCourses(response.data);
+        }).catch(() => {
+            setCourses([]);
+        });
     }, []);
 
-    const handleModifyAccessTime = (courseId: number) => {
-        alert(`Modificación del tiempo de acceso para el curso con ID: ${courseId}`);
+    const onPageChange = (event: any) => {
+        setFirst(event.first);
+        courseService.getCoursesByPage(event.page + 1, 6).then((response) => {
+            setCourses(response.data);
+        });
+    };
+    
+    const openModal = (course: CourseDTO) => {
+        setSelectedCourse(course);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setSelectedCourse(null);
     };
 
     return (
     <div className="content-background">
         <div className="content-container">
-        <CustomSidebarDX
-            sidebarItems={<SidebarItemsAdmin />}
-            mainBackgroundColor="#343A40"
-            headerBackgroundColor="#23272B"
-            headerTextColor="white"
-            headerIconColor="#007BFF"
-        />
-        <div className="presentations-content">
-            <h1>Gestión de Ponencias</h1>
-            <div className="presentations-list">
-            {courses.map((course) => (
-                <div key={course.id} className="presentation-card">
-                <img src={course.banner} alt={`${course.title} banner`} />
-                <div className="presentation-info">
-                    <h2>{course.title}</h2>
-                    <h3>{course.subtitle}</h3>
-                    <p>{course.description}</p>
-                    <div className="presentation-details">
-                    <span>Horas: {course.hours}</span>
-                    <span>Rating: {course.rating}</span>
-                    <span>Precio: PEN {course.price.toFixed(2)}</span>
-                    </div>
-                    <button
-                    className="modify-access-btn"
-                    onClick={() => handleModifyAccessTime(course.id)}
-                    >
-                    Modificar tiempo de acceso
-                    </button>
+            <CustomSidebarDX
+                sidebarItems={<SidebarItemsAdmin />}
+                mainBackgroundColor="#343A40"
+                headerBackgroundColor="#23272B"
+                headerTextColor="white"
+                headerIconColor="#007BFF"
+            />
+            <main className="admin-content">
+                <div className="content">
+                    <h1 className="title">Administración de Eventos</h1>
+                    <section className="section">
+                        <div className="flex-1 p-8 bg-[#343A40]">
+                            <div className="flex align-center justify-between m-5">
+                                <h1 className="text-3xl font-extrabold text-white">Cursos Disponibles</h1>
+                                <Paginator className={"bg-white rounded p-4"} first={first} rows={courses.length > 3 ? courses.length / 3 : 1} totalRecords={10} onPageChange={onPageChange} template={{ layout: 'PrevPageLink CurrentPageReport NextPageLink' }} />
+                            </div>
+                            {courses.length > 0 ? (
+                                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                                    {courses.map((course) => (
+                                        <div key={course.id} className="relative">
+                                            <CourseCard course={course} openModal={openModal} />
+                                        </div>
+                                    ))}
+                                </div>
+                                ) : (
+                                    <div className="text-white  text-center font-bold text-xl lg:text-3xl p-14 md:p-20 lg:p-28  xl:p-32">
+                                        No existen cursos disponibles
+                                    </div>
+                            )}
+                        </div>
+                    </section>
                 </div>
-                </div>
-            ))}
-            </div>
-        </div>
+            </main>
         </div>
     </div>
     );
